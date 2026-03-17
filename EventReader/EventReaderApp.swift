38e815cdfb155c4907a33205264a744183b58c0c
chrisYaoO <20307140015@fudan.edu.cn>
2026-03-17 13:48:00 -0400

    Initial Commit

---
+++ EventReader/EventReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventReaderApp.swift
+//  EventReader
+//
+//  Created by Chris Yao on 2026-03-17.
+//
+
+import SwiftUI
+
+@main
+struct EventReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
